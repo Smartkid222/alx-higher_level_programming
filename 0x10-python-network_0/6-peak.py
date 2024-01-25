c72ce90da1be9965d8c06cf83ed29608f6006b39
Smartkid222 <emekam848@gmail.com>
2024-01-25 08:01:17 -0800

    6-peak.py new

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-'''Finds a peak in a list'''
+"""Finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    '''Finds a peak in a list'''
-    if not list_of_integers:
-        return
-    length = len(list_of_integers)
-    if length == 1:
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
         return list_of_integers[0]
-    peak = list_of_integers[1]
-    a = 1
-    while a < length:
-        if peak < list_of_integers[a]:
-            peak = list_of_integers[a]
-        a += 1
-    return peak
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
